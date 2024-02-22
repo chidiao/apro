0f6b025110e36f0bda7ada509d51d9fab8799c86
孙颖洲 <sunyz94123@qq.com>
2024-02-22 10:45:36 +0800

    feat: 添加商店链接

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,6 +2,24 @@
   <div class="p-4">
     <a-card>
       <svg-banner class="w-full h-auto" />
+
+      <div class="text-4xl text-black text-center font-bold my-16">现在你可以</div>
+
+      <div class="flex justify-center items-center space-x-12 pb-12">
+        <a href="https://play.google.com/store/apps/details?id=com.iris.health" target="_blank">
+          <img
+            src="https://p16-hera-va.ibyteimg.com/tos-useast2a-i-hn4qzgxq2n/551222f67a4e43ac9def49e0f5129edd~tplv-hn4qzgxq2n-webp:500:148.webp"
+            style="display: block; width: 250px; height: 74px"
+          />
+        </a>
+
+        <a href="https://play.google.com/store/apps/details?id=com.iris.health" target="_blank">
+          <img
+            src="https://p16-hera-va.ibyteimg.com/tos-useast2a-i-hn4qzgxq2n/17331dbea41b4d448da089e20349caae~tplv-hn4qzgxq2n-webp:500:148.webp"
+            style="display: block; width: 250px; height: 74px"
+          />
+        </a>
+      </div>
     </a-card>
   </div>
 </template>
